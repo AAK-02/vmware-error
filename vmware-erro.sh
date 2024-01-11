515d77b9fbe1e662eccbac87b864d36a02eb2267
AAK <91734387+AAK-02@users.noreply.github.com>
2024-01-11 22:15:42 +0100

    Add files via upload

---
+++ vmware-erro.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+echo "Don't forget to run it  in the root"
+version=$(vmware --version | awk '{print $3}')
+echo $version
+git clone https://github.com/mkubecek/vmware-host-modules.git
+cd vmware-host-modules
+git checkout workstation-$version
+make
+sudo make install
+sudo /etc/init.d/vmware start
+

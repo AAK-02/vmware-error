f081f032cff2831bfdc1ea9cffc6b248d35c6c91
AAK <91734387+AAK-02@users.noreply.github.com>
2024-01-11 22:22:07 +0100

    Update and rename vmware-erro.sh to vmware-error.sh

--- vmware-error.sh
+++ vmware-error.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 echo "Don't forget to run it  in the root"
 version=$(vmware --version | awk '{print $3}')
-echo $version
 git clone https://github.com/mkubecek/vmware-host-modules.git
 cd vmware-host-modules
 git checkout workstation-$version

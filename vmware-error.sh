76e467fbaf0eb6cfca07b16a14a1f2de9351b624
AAK <91734387+AAK-02@users.noreply.github.com>
2024-02-11 22:44:27 +0100

    Update vmware-error.sh

--- vmware-error.sh
+++ vmware-error.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
+directory="vmware-host-modules"
 echo "Don't forget to run it  in the root"
+if [[ -d ${directory} ]]; then
+        echo "directory found "
+        rm -r ${directory}
+fi
 version=$(vmware --version | awk '{print $3}')
+echo $version
 git clone https://github.com/mkubecek/vmware-host-modules.git
 cd vmware-host-modules
 git checkout workstation-$version
